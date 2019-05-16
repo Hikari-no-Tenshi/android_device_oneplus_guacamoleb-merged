c3d6a85d7a320bf9faa1cf6eb6a7edd4cd9f8093
Sushil Chauhan <sushilchauhan@codeaurora.org>
2019-05-16 10:00:59 -0700

    guacamoleb: Set vendor prop to disable 3d-tonemap on UI
    Set vendor property "vendor.display.disable_ui_3d_tonemap" to
    disable the 3d-tonemap feature for UI layers in display HAL.
    
    CRs-Fixed: 2445385
    Change-Id: I4017de82fe120629d0284710ea37e7eeb1b3c724

--- vendor_props.mk
+++ vendor_props.mk
@@ -130,6 +130,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_hw_recovery_dump=1 \
     vendor.display.disable_inline_rotator=1 \
     vendor.display.disable_scaler=0 \
+    vendor.display.disable_ui_3d_tonemap=1 \
     vendor.display.enable_null_display=0 \
     vendor.display.enable_optimize_refresh=1 \
     ro.sf.lcd_density=420 \
