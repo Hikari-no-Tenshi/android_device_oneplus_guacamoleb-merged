018dc6a3495f8bb6261f4c1f79f74c9000c65409
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-02-21 13:24:18 +0200

    Revert "guacamoleb: Use aosp SurfaceFlinger"
    This reverts commit 52aa44109b6b80470068c1892d1beff4127d2855.

--- vendor_props.mk
+++ vendor_props.mk
@@ -13,11 +13,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.real.device=true \
     vendor.product.device=guacamoleb \
     vendor.product.manufacturer=oneplus
-
-# SurfaceFlinger
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.use_smart_90_for_video=true \
-    ro.surface_flinger.set_display_power_timer_ms=10000 \
-    ro.surface_flinger.set_touch_timer_ms=5000 \
-    ro.surface_flinger.set_idle_timer_ms=9000
