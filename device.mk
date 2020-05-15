dd1313572381013102fd0b93fe1b2435af1979c9
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-05-15 12:01:08 +0300

    guacamoleb: Relocate Surfaceflinger default props

--- device.mk
+++ device.mk
@@ -441,6 +441,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# SurfaceFlinger
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.has_HDR_display=true \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.wcg_composition_dataspace=143261696
+
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
     RemovePackages
