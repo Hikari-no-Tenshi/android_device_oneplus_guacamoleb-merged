65cbf3a2f6bcc8c0927530b5a4624df715061f57
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-09-27 10:08:54 +0300

    guacamoleb: Import qti C2 codecs config from OOS

--- device.mk
+++ device.mk
@@ -385,6 +385,7 @@ PRODUCT_PACKAGES += \
 
 #MEDIA C2
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
