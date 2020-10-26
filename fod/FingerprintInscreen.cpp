5f7e3fb45543b781be72715a5ae17ece3b9c7531
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-10-26 14:58:05 +0200

    guacamoleb: fod: Restore color mode only when fod view was previously shown

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -96,27 +96,31 @@ Return<void> FingerprintInscreen::onRelease() {
 }
 
 Return<void> FingerprintInscreen::onShowFODView() {
+    if (!mFodCircleVisible) {
+        color_mode = std::stoi(android::base::GetProperty("persist.vendor.sys.color_mode", "0"));
+        this->mVendorDisplayService->setMode(16, 0);
+        this->mVendorDisplayService->setMode(17, 0);
+        this->mVendorDisplayService->setMode(18, 0);
+        this->mVendorDisplayService->setMode(20, 0);
+        this->mVendorDisplayService->setMode(21, 0);
+        this->mVendorDisplayService->setMode(17, 1);
+    }
     this->mFodCircleVisible = true;
-    color_mode = std::stoi(android::base::GetProperty("persist.vendor.sys.color_mode", "0"));
-    this->mVendorDisplayService->setMode(16, 0);
-    this->mVendorDisplayService->setMode(17, 0);
-    this->mVendorDisplayService->setMode(18, 0);
-    this->mVendorDisplayService->setMode(20, 0);
-    this->mVendorDisplayService->setMode(21, 0);
-    this->mVendorDisplayService->setMode(17, 1);
     this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
 
     return Void();
 }
 
 Return<void> FingerprintInscreen::onHideFODView() {
+    if (mFodCircleVisible) {
+        this->mVendorDisplayService->setMode(16, 0);
+        this->mVendorDisplayService->setMode(17, 0);
+        this->mVendorDisplayService->setMode(18, 0);
+        this->mVendorDisplayService->setMode(20, 0);
+        this->mVendorDisplayService->setMode(21, 0);
+        this->mVendorDisplayService->setMode(color_mode > 0 ? color_mode : 17, color_mode > 0 ? 1 : 0);
+    }
     this->mFodCircleVisible = false;
-    this->mVendorDisplayService->setMode(16, 0);
-    this->mVendorDisplayService->setMode(17, 0);
-    this->mVendorDisplayService->setMode(18, 0);
-    this->mVendorDisplayService->setMode(20, 0);
-    this->mVendorDisplayService->setMode(21, 0);
-    this->mVendorDisplayService->setMode(color_mode > 0 ? color_mode : 17, color_mode > 0 ? 1 : 0);
     this->mVendorDisplayService->setMode(OP_DISPLAY_AOD_MODE, 0);
     this->mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
     this->mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
