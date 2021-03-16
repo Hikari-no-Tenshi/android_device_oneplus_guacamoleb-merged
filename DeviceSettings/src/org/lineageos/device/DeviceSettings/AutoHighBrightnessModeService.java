47ffb04c45edff8a8629cf00fb1e1d168c8b8d19
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-03-16 16:09:30 +0200

    DeviceSettings: Use Spline interpolation for HBM Autobrightness

--- DeviceSettings/src/org/lineageos/device/DeviceSettings/AutoHighBrightnessModeService.java
+++ DeviceSettings/src/org/lineageos/device/DeviceSettings/AutoHighBrightnessModeService.java
@@ -6,6 +6,8 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.database.ContentObserver;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -18,58 +20,24 @@ import android.os.Looper;
 import android.os.PowerManager;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.util.Spline;
 
 public class AutoHighBrightnessModeService extends Service {
     private static final String HBM_BRIGHTNESS_FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/hbm_brightness";
     private static final String HBM_FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/hbm";
-
-    private final int[][] HBM_AUTOBRIGHTNESS_ARRAY = {
-        new int[]{20000, 10},
-        new int[]{23000, 40},
-        new int[]{26000, 70},
-        new int[]{29000, 100},
-        new int[]{32000, 150},
-        new int[]{35000, 200},
-        new int[]{38000, 300},
-        new int[]{41000, 400},
-        new int[]{44000, 600},
-        new int[]{47000, 800},
-        new int[]{50000, 1023},
-    };
 
     private static boolean mAutoHBMSensorEnabled = false;
     private static boolean mAutoHBMActive = false;
     private static boolean mIsGoingToSleep = false;
     private boolean mIsAutomaticBrightnessEnabled;
     private Handler mHandler;
+    private Spline mHBMLuxToBacklightSpline;
 
     private SensorManager mSensorManager;
     Sensor mLightSensor;
 
-    private int interpolate(int i, int i2, int i3, int i4, int i5) {
-        int i6 = i5 - i4;
-        int i7 = i - i2;
-        int i8 = ((i6 * 2) * i7) / (i3 - i2);
-        int i9 = i8 / 2;
-        int i10 = i2 - i3;
-        return i4 + i9 + (i8 % 2) + ((i10 == 0 || i6 == 0) ? 0 : (((i7 * 2) * (i - i3)) / i6) / i10);
-    }
-
     private float getHBMBrightness(float lux) {
-        int length = HBM_AUTOBRIGHTNESS_ARRAY.length;
-        int i = 0;
-        while (i < length && HBM_AUTOBRIGHTNESS_ARRAY[i][0] < (int)lux) {
-            i++;
-        }
-        if (i == 0) {
-            return HBM_AUTOBRIGHTNESS_ARRAY[0][1];
-        }
-        if (i == length) {
-            return HBM_AUTOBRIGHTNESS_ARRAY[length - 1][1];
-        }
-        int[][] iArr = HBM_AUTOBRIGHTNESS_ARRAY;
-        int i2 = i - 1;
-        return interpolate((int)lux, iArr[i2][0], iArr[i][0], iArr[i2][1], iArr[i][1]);
+        return mHBMLuxToBacklightSpline.interpolate(lux);
     }
 
     public void activateLightSensorRead() {
@@ -91,7 +59,8 @@ public class AutoHighBrightnessModeService extends Service {
             if (mAutoHBMSensorEnabled) {
                 float lux = event.values[0];
                 if (lux > 20000.0f && !mIsGoingToSleep) {
-                    Utils.writeValue(HBM_BRIGHTNESS_FILE, String.valueOf((int)getHBMBrightness(lux)));
+                    int hbm_brightness = Math.round(getHBMBrightness(lux));
+                    Utils.writeValue(HBM_BRIGHTNESS_FILE, String.valueOf(hbm_brightness));
                     mAutoHBMActive = true;
                 } else if (lux < 20000.0f && mAutoHBMActive && !mIsAutomaticBrightnessEnabled) {
                     Utils.writeValue(HBM_BRIGHTNESS_FILE, "0");
@@ -134,6 +103,10 @@ public class AutoHighBrightnessModeService extends Service {
                 com.android.internal.util.crdroid.content.Intent.ACTION_GO_TO_SLEEP);
         registerReceiver(mScreenStateReceiver, screenStateFilter);
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        final Resources res = getApplicationContext().getResources();
+        float[] hbm_brightness = getFloatArray(res.obtainTypedArray(R.array.config_HBMBrightnessBacklight));
+        float[] hbm_lux = getFloatArray(res.obtainTypedArray(R.array.config_HBMautoBrightnessLevels));
+        mHBMLuxToBacklightSpline = Spline.createSpline(hbm_lux, hbm_brightness);
         mCustomSettingsObserver.observe();
         mCustomSettingsObserver.update();
         if (pm.isInteractive()) {
@@ -194,5 +167,15 @@ public class AutoHighBrightnessModeService extends Service {
                 Settings.System.SCREEN_BRIGHTNESS_MODE,
                 Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL,
                 UserHandle.USER_CURRENT) == 1;
+    }
+
+    private static float[] getFloatArray(TypedArray array) {
+        int length = array.length();
+        float[] floatArray = new float[length];
+        for (int i = 0; i < length; i++) {
+            floatArray[i] = array.getFloat(i, Float.NaN);
+        }
+        array.recycle();
+        return floatArray;
     }
 }
