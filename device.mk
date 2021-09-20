8b020c719c4441f84ff0e9e8e1659abf9b8c578a
Bruno Martins <bgcngm@gmail.com>
2021-09-20 18:23:58 +0100

    guacamoleb: Blob out ODM feature list
    There's not much of a purpose to have this in the device tree.
    
    Change-Id: I67751642b2f34576106a39a83e21996010215c3d

--- device.mk
+++ device.mk
@@ -389,10 +389,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.sys.oneplus.support=11.13.1
 
-# OPFeature
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/odm_feature_list:$(TARGET_COPY_OUT_ODM)/etc/odm_feature_list
-
 # OPScreenRecorder
 PRODUCT_PACKAGES += \
     OPScreenRecorder
