c1e43f3b6aa0ce95def7f3f4f69f22471a0fe835
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-11-10 12:28:39 +0200

    guacamoleb: Remove interfaces from device tree
    commit c12e81f92075f8a7945113cfaf207f96aa5bd683
    Author: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Date:   Wed Nov 10 12:26:57 2021 +0200
    
        Revert "guacamoleb: Import OnePlus HAL definitions"
    
        This reverts commit 5ce400d299503a78561d6514d96e0926fde05af9.
    
    commit d5f6ef734a427944d051b725ae62fc3e2a5f5260
    Author: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Date:   Wed Nov 10 12:26:38 2021 +0200
    
        Revert "guacamoleb: Import CameraMDMHIDL HAL"
    
        This reverts commit 17081b63a53719ded85c30d682f98971b64e5ccc.
    
    commit 214b964841895f1fe97f86e99b03c8ad701a487f
    Author: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Date:   Wed Nov 10 12:26:15 2021 +0200
    
        Revert "guacamoleb: interfaces: Regenerate makefiles"
    
        This reverts commit ce536bc67e49709e1acda54bdc4746f7ac7a6b93.
    
    commit 0f42c35c7124338e7c5eabc11682c22775d294a1
    Author: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Date:   Wed Nov 10 12:25:51 2021 +0200
    
        Revert "guacamoleb: Import camera HAL"
    
        This reverts commit 9cfa3d3ceceac1b9c63b86e0feb0ca010748803c.
    
    commit 03b6515bb6d8f831e343218df0f33b7c0cd719e2
    Author: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Date:   Wed Nov 10 12:25:24 2021 +0200
    
        Revert "guacamoleb: Move CameraMDMHIDL to hardware"
    
        This reverts commit 093b51bdb7f73b0b19c4c35b00014cbe80b45d74.

--- device.mk
+++ device.mk
@@ -442,7 +442,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    device/oneplus/common
 
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
