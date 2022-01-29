1339dd3fe4a8caf7adb2b559158632661c2c4694
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2022-01-29 20:07:16 +0200

    guacamoleb: Exclude AudioFX

--- device.mk
+++ device.mk
@@ -143,6 +143,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
+
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-impl-qti \
